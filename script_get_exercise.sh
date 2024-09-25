40a28f0e9ca5e05a20108200f1fc60e282df9efe
GiancarloCroce <giancarlo.croce01@gmail.com>
2024-09-25 15:17:26 +0200

    Fall2024 - ok packages installation

--- script_get_exercise.sh
+++ script_get_exercise.sh
@@ -7,48 +7,56 @@ git clone https://github.com/GfellerLab/exercises_intro_bioinfo_clinique
 cd exercises_intro_bioinfo_clinique
 
 
-#### install package. For each user separately (manual installation)!!!
+#### HOW TO install package.
+## OPTION 1: For each user separately (manual installation)!!!
 # open a jupyter notebook
 # run:
-#!pip install pandas numpy scipy plotly scikit-survival scikit-learn umap h5py scikit-image matplotlib
+#!sudo pip install pandas numpy scipy plotly scikit-survival scikit-learn umap h5py scikit-image matplotlib
 # restart kernel
 # log out and log in again
-# UPDATE: added this line to Week1 jupyter-notebook
+# added this line to Week1 jupyter-notebook
+#
+## OPTION 2:
+# It seems related to this: https://github.com/pypa/pip/issues/11776
+# WHAT I've done: mv EXTERNALLY_MANAGED EXTERNALLY_MANAGED_BKCP
 
 ####################################################################################################
-#copy files to each user (rm solutions)
-cd /home
+#copy files to each user (include/exclude solutions)
 
+cd /home
 week="Week1"
-filename="exercise_week1.ipynb"
+include_solution=true
 echo "Copying file $filename for $week"
-for i in `ls | grep u[00-300]`
+
+for i in $(ls | grep u[00-300])
 do
-	echo $i, $week
-	#making directory
-	mkdir /home/$i/$week
-	cp /home/jupyter-admin0/exercises_intro_bioinfo_clinique/$week/$filename $i/$week
-	sudo chmod 777 $i/$week
+    echo "$i, $week"
+    # Making directory
+    mkdir -p /home/$i/$week
+    
+    if $include_solution; then
+        for filename in $(ls /home/jupyter-admin0/exercises_intro_bioinfo_clinique/$week/)
+        do
+            cp /home/jupyter-admin0/exercises_intro_bioinfo_clinique/$week/$filename /home/$i/$week/
+        done
+    else
+        for filename in $(ls /home/jupyter-admin0/exercises_intro_bioinfo_clinique/$week/ | grep -v solution)
+        do
+            cp /home/jupyter-admin0/exercises_intro_bioinfo_clinique/$week/$filename /home/$i/$week/
+        done
+    fi
+
+    sudo chmod 777 /home/$i/$week
 done
 
 
-#####################################################################################################
-##### copy solution
-#week="Week1"
-#echo "Copying solution for $week "
-#for i in `ls | grep u[00-300]`
-#do
-#	echo $i, $week
-#	cp /home/jupyter-admin0/exercises_intro_bioinfo_clinique/$week/*solution* $i/$week
-#done
-#
 ####################################################################################################
 #function to clean (remove ALL files)
 cd /home
 for i in `ls | grep u[00-300]`
 do
 	echo $i
-	rm -r $i/Week*
+	rm -rf /home/$i/*
 done
 
 
